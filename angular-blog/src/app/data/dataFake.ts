0aec502b44e14aba3faa394ff5de4d14e367831c
nathaliaatamanchuk <nathaliaatamanchuk5@gmail.com>
2023-10-13 18:58:35 -0300

    content page dynamism

--- angular-blog/src/app/data/dataFake.ts
+++ angular-blog/src/app/data/dataFake.ts
@@ -1,12 +1,16 @@
 export const dataFake = [
     {
-        "id":1,
+        "id": "1",
         "photo":"photo1",
-        "title": "description1",
+        "title": "title1",
+        "description": "description1",
+        "photoCover":""
     },
     {
-        "id":2,
+        "id":"2",
         "photo":"photo2",
-        "title": "description2",
+        "title": "title2",
+        "description": "description2",
+        "photoCover":""
     }
 ]
--- angular-blog/src/app/pages/content/content.component.ts
+++ angular-blog/src/app/pages/content/content.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import {dataFake} from '../../data/dataFake'
 
 @Component({
   selector: 'app-content',
@@ -7,12 +8,10 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./content.component.css']
 })
 export class ContentComponent implements OnInit{
-  @Input()
   photoCover:string = ""
-  @Input()
   contentTitle:string = ""
-  @Input()
   contentDescription:string = ""
+  private id:string | null = "0"
 
   constructor(
     private route:ActivatedRoute
@@ -20,7 +19,15 @@ export class ContentComponent implements OnInit{
 
   ngOnInit(): void {
       this.route.paramMap.subscribe( value =>
-        console.log(value.get("id"))
+        this.id = value.get("id")
       )
+  }
+  setValuesToComponent(id:string | null){
+    const result = dataFake.filter(article => article.id == id)[0]
+
+  
+      this.contentTitle = result.title
+      this.contentDescription = result.description
+      this.photoCover = result.photoCover
   }
 }
